ffe34c0551b6ce6a15832db2e54f310070dae4aa
cyqpp-uw <cyqpp@uw.edu>
2022-04-09 16:30:57 -0700

    Configuring Firebase

---
+++ mock_data/mockData.js
@@ -0,0 +1,20 @@
+import useWindowDimensions from "react-native/Libraries/Utilities/useWindowDimensions";
+
+const users = {
+    userId: "12345",
+    email: "12345@uw.edu",
+    firstName: "Bob",
+    lastName: "Ross",
+    UW: true,
+    interests: ["Furniture", "Electronics"],
+    likedItems:["0000000"]
+}
+
+const conversation = {
+
+}
+
+const items = {
+
+}
+
