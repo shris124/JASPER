7184ed661153dd65fbc879b0899c8356e6d84a29
Mariam Gadzhimagomedova <36463118+synKimch@users.noreply.github.com>
2022-04-23 16:51:06 -0700

    A perhaps working Login

--- screens/Login.js
+++ screens/Login.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { SafeAreaView, Text } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { Theme } from '../components';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert
+} from "react-native";
 
 const Login = ({ navigation }) => {
   return (
-    <SafeAreaView>
-      <Text>Sign in Page Here :D</Text>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <TextInput placeholder="Enter your UW ID" placeholderTextColor='#6314AB'></TextInput>
+      <TextInput placeholder="Password" placeholderTextColor='#6314AB' secureTextEntry={true}></TextInput>
+      <Button title='Sign in' onPress={() => Alert.alert('Signing in')}></Button>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+      container: {
+        flex:1,
+        alignItems: "center",
+        justifyContent: "center",
+      }
+});
 
 export default Login;
