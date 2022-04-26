6ae053f0cf4f5d3361f720431750fec36e138890
cyqpp-uw <cyqpp@uw.edu>
2022-04-26 14:03:22 -0700

    Commended out firebase.js to avoid bug

--- firebase.js
+++ firebase.js
@@ -1,100 +1,101 @@
-import { initializeApp } from "firebase/app";
-import {
-    GoogleAuthProvider,
-    getAuth,
-    signInWithPopup,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    sendPasswordResetEmail,
-    signOut,
-} from "firebase/auth";
-import {
-    getFirestore,
-    query,
-    getDocs,
-    collection,
-    where,
-    addDoc,
-} from "firebase/firestore";
-const firebaseConfig = {
-    apiKey: "AIzaSyDoO-15TP38J4Gzz4BlI3rFucgYkymFs04",
-    authDomain: "uwjasper.firebaseapp.com",
-    databaseURL: "https://uwjasper-default-rtdb.firebaseio.com",
-    projectId: "uwjasper",
-    storageBucket: "uwjasper.appspot.com",
-    messagingSenderId: "393140379927",
-    appId: "1:393140379927:web:09b62a59258415ffcad752",
-    measurementId: "G-VQBW4GV8Z1"
-};
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const googleProvider = new GoogleAuthProvider();
-const signInWithGoogle = async () => {
-  try {
-    const res = await signInWithPopup(auth, googleProvider);
-    const user = res.user;
-    const q = query(collection(db, "users"), where("uid", "==", user.uid));
-    const docs = await getDocs(q);
-    if (docs.docs.length === 0) {
-      await addDoc(collection(db, "users"), {
-        uid: user.uid,
-        name: user.displayName,
-        authProvider: "google",
-        email: user.email,
-      });
-    }
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
-};
+// import { initializeApp } from "firebase/app";
+// import firebase from "firebase";
+// import {
+//     GoogleAuthProvider,
+//     getAuth,
+//     signInWithPopup,
+//     signInWithEmailAndPassword,
+//     createUserWithEmailAndPassword,
+//     sendPasswordResetEmail,
+//     signOut,
+// } from "firebase/auth";
+// import {
+//     getFirestore,
+//     query,
+//     getDocs,
+//     collection,
+//     where,
+//     addDoc,
+// } from "firebase/firestore";
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDoO-15TP38J4Gzz4BlI3rFucgYkymFs04",
+//     authDomain: "uwjasper.firebaseapp.com",
+//     databaseURL: "https://uwjasper-default-rtdb.firebaseio.com",
+//     projectId: "uwjasper",
+//     storageBucket: "uwjasper.appspot.com",
+//     messagingSenderId: "393140379927",
+//     appId: "1:393140379927:web:09b62a59258415ffcad752",
+//     measurementId: "G-VQBW4GV8Z1"
+// };
+// const app = firebase.initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+// const googleProvider = new GoogleAuthProvider();
+// const signInWithGoogle = async () => {
+//   try {
+//     const res = await signInWithPopup(auth, googleProvider);
+//     const user = res.user;
+//     const q = query(collection(db, "users"), where("uid", "==", user.uid));
+//     const docs = await getDocs(q);
+//     if (docs.docs.length === 0) {
+//       await addDoc(collection(db, "users"), {
+//         uid: user.uid,
+//         name: user.displayName,
+//         authProvider: "google",
+//         email: user.email,
+//       });
+//     }
+//   } catch (err) {
+//     console.error(err);
+//     alert(err.message);
+//   }
+// };
 
-const logInWithEmailAndPassword = async (email, password) => {
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
-};
+// const logInWithEmailAndPassword = async (email, password) => {
+//   try {
+//     await signInWithEmailAndPassword(auth, email, password);
+//   } catch (err) {
+//     console.error(err);
+//     alert(err.message);
+//   }
+// };
 
-const registerWithEmailAndPassword = async (name, email, password) => {
-  try {
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-    const user = res.user;
-    await addDoc(collection(db, "users"), {
-      uid: user.uid,
-      name,
-      authProvider: "local",
-      email,
-    });
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
-};
+// const registerWithEmailAndPassword = async (name, email, password) => {
+//   try {
+//     const res = await createUserWithEmailAndPassword(auth, email, password);
+//     const user = res.user;
+//     await addDoc(collection(db, "users"), {
+//       uid: user.uid,
+//       name,
+//       authProvider: "local",
+//       email,
+//     });
+//   } catch (err) {
+//     console.error(err);
+//     alert(err.message);
+//   }
+// };
 
-const sendPasswordReset = async (email) => {
-  try {
-    await sendPasswordResetEmail(auth, email);
-    alert("Password reset link sent!");
-  } catch (err) {
-    console.error(err);
-    alert(err.message);
-  }
-};
+// const sendPasswordReset = async (email) => {
+//   try {
+//     await sendPasswordResetEmail(auth, email);
+//     alert("Password reset link sent!");
+//   } catch (err) {
+//     console.error(err);
+//     alert(err.message);
+//   }
+// };
 
-const logout = () => {
-  signOut(auth);
-};
+// const logout = () => {
+//   signOut(auth);
+// };
 
-export {
-  auth,
-  db,
-  signInWithGoogle,
-  logInWithEmailAndPassword,
-  registerWithEmailAndPassword,
-  sendPasswordReset,
-  logout,
-};
+// export {
+//   auth,
+//   db,
+//   signInWithGoogle,
+//   logInWithEmailAndPassword,
+//   registerWithEmailAndPassword,
+//   sendPasswordReset,
+//   logout,
+// };
